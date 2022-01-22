6db20ed385338e72d4dea8693cccf723e7574310
Max Hughes <mhughes2011@gmail.com>
2022-01-22 15:40:18 -0800

    added clearUsers functionality

--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -4,7 +4,7 @@ import GithubContext from '../../context/github/GithubContext'
 function UserSearch() {
     const [text, setText] = useState('')
 
-    const {users, searchUsers} = useContext(GithubContext)
+    const {users, searchUsers, clearUsers} = useContext(GithubContext)
 
     const handleChange = (e) => {
         setText(e.target.value)
@@ -20,6 +20,11 @@ function UserSearch() {
             setText('')
         }
     }
+
+    const handleClear = () => {
+        clearUsers()
+    }
+    // The handleClear function isn't needed because you can just call the clearUsers function directly from context in the onClick down on the button.  This way works too lol
 
     return (
         <div className='grid grid-cols-1 xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-2 mb-8 gap-8'>
@@ -46,7 +51,7 @@ function UserSearch() {
             </div>
             {users.length > 0 && (
                 <div>
-                    <button className="btn btn-ghost btn-lg">
+                    <button onClick={handleClear} className="btn btn-ghost btn-lg">
                         Clear
                     </button>
                 </div>
--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -32,13 +32,21 @@ export const GithubProvider = ({children}) => {
 
     }
 
+    // Clear the search results (users from state)
+    const clearUsers = () => {
+        dispatch({
+            type: 'CLEAR_USERS'
+        })
+    }
+
     // Set Loading function to use anywhere
     const setLoading = () => dispatch({type: 'SET_LOADING'})
     
     return <GithubContext.Provider value = {{
         users: state.users,
         loading: state.loading,
-        searchUsers
+        searchUsers,
+        clearUsers
     }}>
         {children}
     </GithubContext.Provider>   
